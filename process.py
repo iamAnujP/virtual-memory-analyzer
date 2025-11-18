d60c6846c43c40a883377cee9503921fdad97b60
iamAnujP <pandeanuj12@gmail.com>
2025-11-18 11:03:35 +0530

    process.py

--- process.py
+++ process.py
@@ -5,17 +5,17 @@ class Process:
 
     def access_pages(self, sequence):
         self.page_sequence = sequence
-        print(f"🧩 Process accessing pages: {sequence}")
+        print(f" Process accessing pages: {sequence}")
         for page in sequence:
             self.memory_manager.access_page(page)
 
     def simulate(self, sequence):
         self.page_sequence = sequence
         page_faults = 0
-        print(f"🚀 Starting simulation for sequence: {sequence}")
+        print(f" Starting simulation for sequence: {sequence}")
         for page in sequence:
             self.memory_manager.access_page(page)
             if self.memory_manager.last_fault:
                 page_faults += 1
-        print(f"✅ Simulation complete. Total page faults: {page_faults}")
+        print(f" Simulation complete. Total page faults: {page_faults}")
         return page_faults
